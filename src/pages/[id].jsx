1267700a878ae52ad30b1c135c240682c0fe7c41
trendsandtactics <trendsntactics@gmail.com>
2026-03-21 16:07:14 +0000

    id

--- src/pages/[id].jsx
+++ src/pages/[id].jsx
@@ -3,6 +3,7 @@ import { useRouter } from 'next/router';
 import Image from 'next/image';
 import Header from '@/src/layout/headers/header';
 import Breadcrumb from '@/src/common/breadcrumb/breadcrumb';
+import Link from 'next/link';
 import FooterContact from '@/src/layout/footers/footer-contact';
 import FooterThree from '@/src/layout/footers/footer-3';
 import { offering_data } from '@/src/data/offering-data';
@@ -33,7 +34,24 @@ const ServiceDetails = () => {
         <Breadcrumb top_title="Service Details" page_title="Service Details" />
         
         <div className="container pt-120 pb-120">
-          <div className="row justify-content-center">
+          <div className="row">
+            <div className="col-lg-4">
+              <div className="tp-service-widget">
+                <div className="tp-service-widget-item mb-40">
+                  <div className="tp-service-widget-tab">
+                    <ul>
+                      {offering_data.map((item, i) => (
+                        <li key={i}>
+                          <Link className={item.slug === id ? "active" : ""} href={`/${item.slug}`}>
+                            {item.title} <i className="fa-regular fa-arrow-right-long"></i>
+                          </Link>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
             <div className="col-lg-8 text-center">
               <div className="service-details-thumb mb-40">
                 <Image src={service.img} alt="theme-pure" width={200} height={200} style={{ objectFit: 'contain' }} />
