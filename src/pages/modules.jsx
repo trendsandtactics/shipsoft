c93f24da8fafc770eb578ca21a2950f3fa540624
trendsandtactics <trendsntactics@gmail.com>
2026-03-22 10:54:05 +0000

    nod

--- src/pages/modules.jsx
+++ src/pages/modules.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Wrapper from '../layout/wrapper';
 import SEO from '../common/seo';
 import Header from '../layout/headers/header';
-import OfferingArea from '../components/homes/home-2/offering-area';
+import OfferingArea from '../components/homes/home-2/offer-area';
 import FooterContact from '../layout/footers/footer-contact';
 import FooterThree from '../layout/footers/footer-3';
 
