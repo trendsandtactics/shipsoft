4516d3c3c225912fb973746b516ee863fcd5111a
trendsandtactics <trendsntactics@gmail.com>
2026-03-22 22:46:27 +0530

    Update index.jsx

--- src/components/blog/index.jsx
+++ src/components/blog/index.jsx
@@ -3,6 +3,7 @@ import HeaderOne from "@/src/layout/headers/header";
 import React from "react";
 import PostboxArea from "./postbox-area";
 import FooterContact from "@/src/layout/footers/footer-contact";
+import BlogArea from "./blog-area";
 import FooterThree from "@/src/layout/footers/footer-3";
 
 const Blog = () => {
@@ -11,7 +12,7 @@ const Blog = () => {
       <HeaderOne />
       <main>
         <Breadcrumb top_title="Blog Page" page_title="Blog" />
-        <PostboxArea />
+        <BlogArea />
         <FooterContact bg_style={true} />
       </main>
       <FooterThree />
